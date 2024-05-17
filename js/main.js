b3985b58ee418ddb1d3de6c1b65b5c454e8fb295
Masimka55 <146752851+Masimka55@users.noreply.github.com>
2024-05-17 15:17:47 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+
+const hamburger = document.querySelector(".menu-navigation");
+const navMenu = document.querySelector(".nav-list"); 
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("open"); 
+} 
+
+$(document).ready(function(){
+    $('.slider-wrapper').slick({
+        arrows:true, 
+        dots:true,
+      
+        variableWidth: true,
+        slidesToShow:3, 
+		prevArrow: $('.buttons-castom__prev'),
+		nextArrow: $('.buttons-castom__next'),
+		
+          responsive:[{
+            breakpoint:768,
+            settings:{
+                slidesToShow:2,
+            },
+            breakpoint:480,
+            settings:{
+                slidesToShow:1,
+            }
+          }],
+
+         
+    });
+  });
+  
+		  
+
+
+		  
